41b07b13212e963a335f08e2eb58fb68a69d891e
Duck <suphloeksangko@gmail.com>
2026-04-28 12:49:25 +0700

    Update app_secrets.py

--- app_secrets.py
+++ app_secrets.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 import os
 
-LINE_CHANNEL_ACCESS_TOKEN = "npDbdiA7FPORzL3LrtUnahsa4mPImx8mVSpWPON/RTT/VzOU+3IwqmPYfj0Tip4XdmgnbMZ86QddoufkGs94vbyJWStWvHZxcCOZPuttCLLlFT2e3SHy6TW8a4xVeBt10LV+y2Nh7pn3jOUSfOcYWAdB04t89/1O/w1cDnyilFU="
-LINE_PUSH_TO = "U986fb0aeca4be80b25d681ddbc2d68a6"
+LINE_CHANNEL_ACCESS_TOKEN = ""
+LINE_PUSH_TO = ""
 
 
 def get_line_channel_access_token() -> str:
